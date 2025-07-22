c888f5a30cf61d049744964e6d4766b0a48b289e
marizari <marizari2006@gmail.com>
2025-07-22 01:42:26 -0300

    estruturando arena.html

--- assets/js/script.js
+++ assets/js/script.js
@@ -10,3 +10,23 @@ document.querySelectorAll('.team').forEach(team => {
   });
 });
 
+function listarGuerreiros() {
+  const lista = document.getElementById("lista-guerreiros");
+  const guerreiros = JSON.parse(localStorage.getItem("guerreiros")) || [];
+
+  if (guerreiros.length === 0) {
+    lista.innerHTML = '<li>Nenhum guerreiro alistado ainda.</li>';
+    return;
+  }
+
+  
+  guerreiros.sort((a, b) => b.vitorias - a.vitorias);
+
+  lista.innerHTML = guerreiros.map((g, i) =>
+    `<li>#${i + 1} ${g.nome} <span>— ${g.classe} da Casa ${g.casa} | Vitórias: ${g.vitorias}</span></li>`
+  ).join('');
+}
+
+window.addEventListener("DOMContentLoaded", listarGuerreiros);
+
+
